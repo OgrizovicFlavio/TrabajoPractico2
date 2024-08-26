03c19923a0c354a4a0637747c271bd309e18ccff
Flavio Ogrizovic <flavio.ogrizovic@gmail.com>
2024-08-26 19:42:37 -0300

    Include SpawnManager for obstacles

---
+++ Assets/Scripts/SpawnManager/SpawnManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject obstaclePrefab;
+    [SerializeField] private float timeToSpawn;
+
+    private float totalTime;
+    private GameObject obstacle;
+
+    private void Start()
+    {
+        SetRandomTimeToSpawn();
+        totalTime = 0;
+    }
+    void Update()
+    {
+        totalTime += Time.deltaTime;
+        if (totalTime > timeToSpawn)
+        {
+            SpawnObstacle();
+            totalTime = 0;
+            SetRandomTimeToSpawn();
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        Vector2 randomPosition = new Vector2(Random.Range(-2f, 2f), Random.Range(-2f, 2f)); //Creo un vector random (x,y).
+
+        GameObject obstacle = Instantiate(obstaclePrefab, randomPosition, Quaternion.identity); //Lo paso como la posicion del nuevo obstáculo.
+
+        float destroyTime = Random.Range(3f, 7f); //Calculo entre 3 y 7 s el tiempo para que se destruya.
+
+        Destroy(obstacle, destroyTime); //Se destruye el objeto
+
+    }
+
+    private void SetRandomTimeToSpawn()
+    {
+        timeToSpawn = Random.Range(3f, 5f);
+    }
+}
