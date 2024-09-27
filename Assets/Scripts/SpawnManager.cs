7c44c069bb29d37768ebfef50eecfcc17a202138
Flavio Ogrizovic <flavio.ogrizovic@gmail.com>
2024-09-27 20:09:38 -0300

    Update SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -12,7 +12,7 @@ public class SpawnManager : MonoBehaviour // Este script va a ser para los Power
 
     private void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++) //Instancio 10 obstáculos y los agrego a mi lista de obstáculos. Los inicializo desactivados y en el centro de la escena.
         {
             Obstacle obstacle = Instantiate(obstaclePrefab, Vector3.zero, Quaternion.identity); //Lo paso como la posicion del nuevo obstáculo.
             obstacles.Add(obstacle);
@@ -27,7 +27,7 @@ public class SpawnManager : MonoBehaviour // Este script va a ser para los Power
             return;
 
         totalTime += Time.deltaTime;
-        if (totalTime > timeToSpawn)
+        if (totalTime > timeToSpawn) //Spawneo de obstáculos de acuerdo al tiempo transcurrido. Si el tiempo transcurrido supera el tiempo de spawneo (aleatorio), se spawnea el objeto.
         {
             SpawnObstacle();
             totalTime = 0;
@@ -37,19 +37,19 @@ public class SpawnManager : MonoBehaviour // Este script va a ser para los Power
 
     private void SpawnObstacle()
     {
-        Vector2 randomPosition;
-        bool isBlockedPosition = false;
-        float radius = 1f;
+        Vector2 randomPosition; //Posición aleatoria donde aparecerá el objeto.
+        bool isBlockedPosition = false; //Variable bandera para chequear si está bloqueada la posición por otro obstáculo.
+        float radius = 1f; //Establezco una distancia mínima entre los obstáculos.
         do
         {
             isBlockedPosition = false;
             randomPosition = new Vector2(Random.Range(-2f, 2f), Random.Range(-2f, 2f));
             for (int i = 0; i < obstacles.Count; i++)
             {
-                if (obstacles[i].gameObject.activeSelf)
+                if (obstacles[i].gameObject.activeSelf) //Si está activo...
                 {
                     float distance = Vector3.Distance(randomPosition, obstacles[i].transform.position);
-                    if(distance < radius * 2)
+                    if(distance < radius * 2) //Si la distancia es menor que el diámetro de un obstáculo, significa que las posiciones se solapan. La bandera se activa y se corta el bucle.
                     {
                         isBlockedPosition = true;
                         break;
@@ -59,9 +59,9 @@ public class SpawnManager : MonoBehaviour // Este script va a ser para los Power
         } while (isBlockedPosition);
         for (int i = 0;i < obstacles.Count; i++)
         {
-            if (!obstacles[i].gameObject.activeSelf)
+            if (!obstacles[i].gameObject.activeSelf) //Busco en mi lista de obstáculos uno que esté no esté activo.
             {
-                obstacles[i].TurnOn(randomPosition);
+                obstacles[i].TurnOn(randomPosition); //Activo el obstáculo en la posición aleatoria.
                 break;
             }
         }
